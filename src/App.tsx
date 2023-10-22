051098e913acb84aea9b2a2f665ed51586772fe7
Bartosz <bartos.pindel@gmail.com>
2023-10-23 00:17:59 +0200

    add bg image

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@
 // pics = https://dog.ceo/dog-api/documentation/
 // facts = https://dogapi.dog/docs/api-v2
 
-import { Container } from "reactstrap";
+import { Card, CardHeader, Container } from "reactstrap";
+import dogsBg from "./assets/dogsBg.jpg";
 
 function App() {
-  return <Container>Hi</Container>;
+  return (
+    <div style={{ backgroundImage: dogsBg }}>
+      {/* <div> */}
+      <Card>
+        <h1>DOGS!</h1>
+        <h2>We love them, we care about them</h2>
+        <em>
+          Unless you are from The Cat Camp. Then you are forgiven (for your lack
+          of taste).
+        </em>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
