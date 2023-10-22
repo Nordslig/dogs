cf5142e673ccf4fad051455e779902850945e0a4
Bartosz <bartos.pindel@gmail.com>
2023-10-22 23:37:06 +0200

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// dogs api:
+// pics = https://dog.ceo/dog-api/documentation/
+// facts = https://dogapi.dog/docs/api-v2
+
+import { Container } from "reactstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Container>Hi</Container>;
 }
 
 export default App;
