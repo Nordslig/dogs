4163a9d88ad9d7beb8e9d963912a98e32f19d1f6
Bartosz <bartos.pindel@gmail.com>
2023-10-30 22:32:48 +0100

    start adding list of breeds, get image of chosen dog

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,14 +1,37 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "reactstrap";
 
-const Form = () => {
-  const getBreeds = async () => {
-    const res = await axios.get("https://dogapi.dog/api/v2/breeds");
+// * dogs api:
+// * pics = https://dog.ceo/dog-api/documentation/
+// * facts = https://dogapi.dog/docs/api-v2
 
-    console.log(res.data.data);
-    for (let i = 0; i < res.data.data.length; i++) {
-      console.log(res.data.data[i].attributes.name);
+const Form = () => {
+  const [dogImage, setDogImage] = useState<"" | undefined>("");
+  const [breedsList, setBreedsList] = useState<string[]>([]);
+
+  useEffect(() => {
+    const getInfo = async () => {
+      const getPagesNum = await axios.get("https://dogapi.dog/api/v2/breeds/");
+      console.log(getPagesNum.data.links.last);
+    };
+
+    getInfo();
+  }, []);
+
+  const getBreeds = async () => {
+    const factsRes = await axios.get(
+      "https://dogapi.dog/api/v2/breeds/3d6b889f-9c5e-4b31-bb46-60280beeb663"
+    );
+    const imageRes = await axios.get(
+      `https://dog.ceo/api/breed/pug/images/random`
+    );
+
+    console.log(factsRes.data.data);
+
+    setDogImage(imageRes.data.message);
+    for (let i = 0; i < factsRes.data.data.length; i++) {
+      console.log(factsRes.data.data[i].attributes.name);
     }
   };
 
@@ -17,11 +40,14 @@ const Form = () => {
   }, []);
 
   return (
-    <form>
-      <label></label>
-      <input name="" type="text" />
-      <Button>Search for dog</Button>
-    </form>
+    <>
+      <form>
+        <label htmlFor="dogBreed">Dog breed: </label>
+        <Button onClick={getBreeds}>PUG</Button>
+        {/* <Button>Search for dog</Button> */}
+      </form>
+      <img src={dogImage} />
+    </>
   );
 };
 
