472bab733ef9cb96b49b9e760f468d546f02f469
Bartosz <bartos.pindel@gmail.com>
2023-11-04 21:43:24 +0100

    choose group, get all breeds from it, display their names,

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,15 +8,67 @@ import { Button } from "reactstrap";
 
 const Form = () => {
   const [dogImage, setDogImage] = useState<"" | undefined>("");
+  const [groups, setGroups] = useState<string[]>([]);
   const [breedsList, setBreedsList] = useState<string[]>([]);
+
+  const breedsNames: string[] = [];
+
+  const getGroups = async (groupNum: number) => {
+    const getGroups = await axios.get("https://dogapi.dog/api/v2/groups");
+    // console.log(getGroups.data.data[4].relationships.breeds);
+    // ! TEST THIS setGroups([]);
+    const groups: string[] = [];
+    getGroups.data.data[groupNum].relationships.breeds.data.forEach(
+      (breedId: { id: string; type: string }) => {
+        groups.push(breedId.id);
+      }
+    );
+    setGroups(groups);
+    // groups.splice(0, groups.length);
+  };
+
+  useEffect(() => {
+    getGroups(1);
+  }, []);
+
+  useEffect(() => {
+    const getBreeds = async () => {
+      for (let i = 0; i < groups.length; i++) {
+        const getBreeds = await axios.get(
+          `https://dogapi.dog/api/v2/breeds/${groups[i]}`
+        );
+        setBreedsList((prevState) => [
+          ...prevState,
+          getBreeds.data.data.attributes.name,
+        ]);
+      }
+    };
+    getBreeds();
+  }, [groups]);
 
   useEffect(() => {
     const getInfo = async () => {
-      const getPagesNum = await axios.get("https://dogapi.dog/api/v2/breeds/");
-      console.log(getPagesNum.data.links.last);
-    };
+      const getBreedsInfo = await axios.get(
+        "https://dogapi.dog/api/v2/breeds/"
+      );
+      const linkNumberOfPages = getBreedsInfo.data.links.last;
 
-    getInfo();
+      const numberOfPages = linkNumberOfPages.slice(
+        linkNumberOfPages.indexOf("[number]=") + "[number]=".length
+      );
+
+      for (let i = 1; i <= numberOfPages; i++) {
+        const getBreedsNames = await axios.get(
+          `https://dogapi.dog/api/v2/breeds?page[number]=${i}`
+        );
+
+        getBreedsNames.data.data.forEach((breed: any) => {
+          breedsNames.push(breed.attributes.name);
+          console.log(breedsNames.sort());
+        });
+      }
+    };
+    // getInfo();
   }, []);
 
   const getBreeds = async () => {
@@ -41,9 +93,12 @@ const Form = () => {
 
   return (
     <>
+      <p>{breedsList}</p>
       <form>
         <label htmlFor="dogBreed">Dog breed: </label>
-        <Button onClick={getBreeds}>PUG</Button>
+        <Button onClick={() => getGroups(1)}>1st Group</Button>
+        <Button onClick={() => getGroups(2)}>2nd Group</Button>
+        {/* <Button onClick={getBreeds}>PUG</Button> */}
         {/* <Button>Search for dog</Button> */}
       </form>
       <img src={dogImage} />
