2a8c2a781b78183335b25b357e029e3741ecfc3c
Bartosz <bartos.pindel@gmail.com>
2023-11-19 23:21:00 +0100

    add spinners to indicate loading, breeds display after fetch all of them,

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -8,6 +8,7 @@ import {
   Label,
   List,
   ListInlineItem,
+  Spinner,
 } from "reactstrap";
 
 // * dogs api:
@@ -15,90 +16,14 @@ import {
 // * facts = https://dogapi.dog/docs/api-v2
 
 const Form = () => {
-  const [dogImage, setDogImage] = useState<"" | undefined>("");
   const [groups, setGroups] = useState<{ id: string; name: string }[]>([]);
+  const [chosenGroup, setChosenGroup] = useState<string | null>(null);
+
   const [breedsList, setBreedsList] = useState<string[]>([]);
 
-  const breedsNames: string[] = [];
+  const [dogImage, setDogImage] = useState<"" | undefined>("");
 
-  const getGroups = async (groupNum: number) => {
-    const getGroups = await axios.get("https://dogapi.dog/api/v2/groups");
-    // console.log(getGroups.data.data[4].relationships.breeds);
-    // ! TEST THIS setGroups([]);
-    const groups: string[] = [];
-    getGroups.data.data[groupNum].relationships.breeds.data.forEach(
-      (breedId: { id: string; type: string }) => {
-        groups.push(breedId.id);
-      }
-    );
-    // setGroups(groups);
-    // groups.splice(0, groups.length);
-  };
-
-  useEffect(() => {
-    // getGroups(1);
-  }, []);
-
-  useEffect(() => {
-    const getBreeds = async () => {
-      setBreedsList([]);
-      for (let i = 0; i < groups.length; i++) {
-        const getBreeds = await axios.get(
-          `https://dogapi.dog/api/v2/breeds/${groups[i]}`
-        );
-        setBreedsList((prevState) => [
-          ...prevState,
-          getBreeds.data.data.attributes.name,
-        ]);
-      }
-    };
-    // getBreeds();
-  }, [groups]);
-
-  useEffect(() => {
-    const getInfo = async () => {
-      const getBreedsInfo = await axios.get(
-        "https://dogapi.dog/api/v2/breeds/"
-      );
-      const linkNumberOfPages = getBreedsInfo.data.links.last;
-
-      const numberOfPages = linkNumberOfPages.slice(
-        linkNumberOfPages.indexOf("[number]=") + "[number]=".length
-      );
-
-      for (let i = 1; i <= numberOfPages; i++) {
-        const getBreedsNames = await axios.get(
-          `https://dogapi.dog/api/v2/breeds?page[number]=${i}`
-        );
-
-        getBreedsNames.data.data.forEach((breed: any) => {
-          breedsNames.push(breed.attributes.name);
-          console.log(breedsNames.sort());
-        });
-      }
-    };
-    // getInfo();
-  }, []);
-
-  const getBreeds = async () => {
-    const factsRes = await axios.get(
-      "https://dogapi.dog/api/v2/breeds/3d6b889f-9c5e-4b31-bb46-60280beeb663"
-    );
-    const imageRes = await axios.get(
-      `https://dog.ceo/api/breed/pug/images/random`
-    );
-
-    console.log(factsRes.data.data);
-
-    setDogImage(imageRes.data.message);
-    for (let i = 0; i < factsRes.data.data.length; i++) {
-      console.log(factsRes.data.data[i].attributes.name);
-    }
-  };
-
-  useEffect(() => {
-    // getBreeds();
-  }, []);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const fetchGroups = async () => {
     const response = await axios.get(`https://dogapi.dog/api/v2/groups`);
@@ -119,10 +44,13 @@ const Form = () => {
     const response = await axios.get(`https://dogapi.dog/api/v2/groups/${id}`);
 
     // TODO display list of breeds, then after picking one out, look for informations and pic
-
+    setChosenGroup(response.data.data.attributes.name);
     setBreedsList([]);
+    setIsLoading(true);
 
     const breeds = response.data.data.relationships.breeds.data;
+
+    const breedsListTemp: string[] = [];
 
     for (let i = 0; i < breeds.length; i++) {
       const breed = breeds[i];
@@ -131,23 +59,17 @@ const Form = () => {
         `https://dogapi.dog/api/v2/breeds/${breed.id}`
       );
 
-      setBreedsList((prevState) => [
-        ...prevState,
-        res.data.data.attributes.name,
-      ]);
+      breedsListTemp.push(res.data.data.attributes.name);
     }
-
-    // setBreedsList(response.data.data.relationships.breeds.data)
+    setIsLoading(false);
+    setBreedsList(breedsListTemp);
   };
-  // console.log(breedsList);
 
-  // TODO ----- => chosen group \/
-
-  // TODO improve displaying options
   return (
     <Card>
       <FormGroup>
         <label htmlFor="dogBreed">Dog breed: </label>
+        {groups.length === 0 && <Spinner />}
         {groups.length > 0 && (
           <List>
             {groups.map(({ id, name }) => (
@@ -158,10 +80,13 @@ const Form = () => {
           </List>
         )}
       </FormGroup>
+      {isLoading && <Spinner />}
       {breedsList.length > 0 && (
         <FormGroup>
           <FormGroup>
-            <Label for="selectBreed">Select breed from ----- group</Label>
+            <Label for="selectBreed">
+              Select breed from {chosenGroup} group
+            </Label>
             <Input type="select" id="selectBreed">
               {breedsList.map((breed) => (
                 <option key={breed}>{breed}</option>
