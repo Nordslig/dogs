8655da5b7df1d29f0e6412b1cd1af6281ff029a6
Bartosz <bartos.pindel@gmail.com>
2024-02-09 00:08:43 +0100

    one word

--- src/components/Usage.tsx
+++ src/components/Usage.tsx
@@ -5,10 +5,10 @@ const Usage = () => {
     <div className={styles.usage}>
       <h3>How to use dog finder:</h3>
       <p>
-        Choose one of the seven groups of dog breeds. Then wait until list is
-        shown with specific breeds and select one that you are interested in.
-        After a quick moment, you will see card with information and random
-        image if it exist.
+        Choose one of the groups of dog breeds. Then wait until list is shown
+        with specific breeds and select one that you are interested in. After a
+        quick moment, you will see card with information and random image if it
+        exist.
       </p>
     </div>
   );
